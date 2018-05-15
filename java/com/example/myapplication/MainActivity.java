3495173bfed3195e96ebc1f16dc3fd16b3460cdd
lfreezing <freezing.lu@hotmail.com>
2018-05-15 16:40:59 +0800

    develop

---
+++ java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.myapplication;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.location.Location;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import java.lang.ref.WeakReference;
+
+public class MainActivity extends AppCompatActivity {
+    TextView text;
+    int cnt = 0, toSave = 0;
+    MyApp ma;
+    Location lo;
+    Resources rs;
+    String str;
+
+    public static class MyHandler extends Handler {
+        private final WeakReference<MainActivity> mMActivity;
+
+        MyHandler(MainActivity mactivity){
+            mMActivity = new WeakReference<>(mactivity);
+        }
+
+        @Override
+        public void handleMessage(Message msg) {
+            MainActivity activity = mMActivity.get();
+            if (activity == null)
+                return;
+            switch (msg.what){
+                case 1:
+                    activity.text = activity.findViewById(R.id.main);
+                    activity.lo = (Location) msg.obj;
+                    activity.text.setText(String.format(activity.str, msg.obj));
+                    activity.cnt++;
+                    if (activity.toSave > 0)
+                        activity.toSave--;
+                    break;
+            }
+            super.handleMessage(msg);
+        }
+    }
+
+    MyHandler hl = new MyHandler(this);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        rs = getApplicationContext().getResources();
+        str = rs.getString(R.string.location);
+        ma = (MyApp) getApplication();
+        ma.setHandler(hl);
+    }
+}
---
+++ java/com/example/myapplication/MyApp.java
@@ -0,0 +1,95 @@
+package com.example.myapplication;
+
+//import android.Manifest;
+import android.Manifest;
+import android.app.ActivityManager;
+import android.app.Application;
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.media.AudioManager;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+//import static android.support.v4.content.PermissionChecker.PERMISSION_DENIED;
+
+public class MyApp extends Application{
+    AudioManager am;
+    Location lo = null;
+    LocationManager lm = null;
+    Timer volumeTimer = new Timer();
+    Handler mhl;
+
+    TimerTask volumeTask = new TimerTask() {
+        @Override
+        public void run() {
+            int vol, volMax, permission;
+            Message msg;
+            vol = am.getStreamVolume(AudioManager.STREAM_RING);
+            volMax = am.getStreamMaxVolume(AudioManager.STREAM_RING);
+            Log.i("MyApp", "volume:" + vol + "max:" + volMax);
+            am.setStreamVolume(AudioManager.STREAM_RING, 3, 0);
+            permission = checkPermission(Manifest.permission.ACCESS_FINE_LOCATION,
+                    android.os.Process.myPid(), android.os.Process.myUid());
+            if (permission != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+            lo = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            Log.i("MyApp", "loc" + lo.getLatitude() + ", " + lo.getLongitude());
+            if (mhl != null) {
+                msg = mhl.obtainMessage();
+                msg.what = 1;
+                msg.obj = lo;
+                mhl.sendMessage(msg);
+            }
+        }
+    };
+
+    LocationListener ll = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            Log.i("MyApp", "location changed " + location);
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    };
+
+    void setHandler(Handler hl) {
+        mhl = hl;
+    }
+
+    Handler getHandler() {
+        return mhl;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.i("MyApp", "in myapp");
+
+        lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        am = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        volumeTimer.schedule(volumeTask, 0, 10000);
+    }
+}
